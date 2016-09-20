60c8bef6ac4f2d7e1b5f29bc10fde390022c1879
James McKay <j.mckay14@imperial.ac.uk>
2016-09-19 22:08:48 -0400

    added plotting routine and list for application example 	new file:   models/MDM/example_list.txt 	new file:   src/plot_example.py

---
+++ src/plot_example.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import numpy as np
+from numpy import *
+from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from scipy import integrate
+from scipy import special
+
+from matplotlib import ticker
+from matplotlib.ticker import ScalarFormatter
+
+
+fig=plt.figure()
+
+ax = fig.add_subplot(1,1,1)
+ax.set_xscale('log')
+
+ax.xaxis.set_major_formatter(ticker.FormatStrFormatter("%d"))
+
+A=np.genfromtxt('models/MDM/output/mass_splittings.txt',usecols=[0,1,2])
+x=A[:,0]
+y1=A[:,1]*1000
+y2=A[:,2]*1000
+
+
+plt.plot(x,y2,'-',color='black',label='Explicit pole mass') #
+
+plt.plot(x,y1,'--',color='black',label='Implicit pole mass') #
+
+
+
+xlabel(r"Degenerate mass $M$ (GeV)",fontsize=16)
+ylabel(r"$\Delta M$ (Mev)",fontsize=16)
+plt.ylim([-200,200])
+plt.legend(loc=2)
+
+
+plt.axvspan((9.4e3-0.47e3), (9.4e3+0.47e3), alpha=0.8, color='green')
+
+plt.plot((9.4e3,9.4e3),(0,700),color='red')
+
+plt.savefig("mass_splittings.eps")
