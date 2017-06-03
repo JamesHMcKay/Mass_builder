912e9a826992553849e960c4318a1f7b60d21dbf
James McKay <j.mckay14@imperial.ac.uk>
2017-06-03 22:20:30 +0200

    moved setting of TSIL path to a cmake flag and clean config script so that it is simply moving around some files prior to set up, and also doubles as the cleaning script for removing generated code 	modified:   CMakeLists.txt 	new file:   include/cmake_variables.hpp 	new file:   include/cmake_variables.hpp.in 	deleted:    scripts/clean.sh 	modified:   scripts/config.sh 	deleted:    scripts/config_linux.sh

---
+++ include/cmake_variables.hpp
@@ -0,0 +1,13 @@
+#ifndef __cmake_variables_hpp__
+#define __cmake_variables_hpp__
+
+
+/// Found Mathematica
+#define HAVE_MATHEMATICA 1
+#define MATHEMATICA_WSTP_H "/Applications/Mathematica.app/Contents/SystemFiles/Links/WSTP/DeveloperKit/MacOSX-x86-64/CompilerAdditions/wstp.framework/Headers/wstp.h"
+#define MATHEMATICA_KERNEL "/Applications/Mathematica.app/Contents/MacOS/WolframKernel"
+
+// Found TSIL
+#define TSIL_PATH "/Users/jamesmckay/Documents/Programs/tsil-1.3//tsil_cpp.h"
+
+#endif //// #defined__cmake_variables_hpp__
--- scripts/config.sh
+++ scripts/config.sh
@@ -1,55 +1,32 @@
 #!/bin/sh
 # This should be run in root Mass_builder directory
 
-#cd <system_dependent>/Mass_builder/
-
-echo "Please enter path to the folder containing MathematicaScript and MathKernel, use one of the default options below by entering the corresponding shortcut"
-echo "  "
-echo "osx    for OSX:/Applications/Mathematica.app/Contents/MacOS"
-echo "math   for most other systems which already have kernal from /usr/local/bin/math"
-read MATH_PATH
-if [ "$MATH_PATH" == "osx" ]; then
-  MATH_PATH="/Applications/Mathematica.app/Contents/MacOS"
+if [ -f "src/self_energy.cpp" ]; then
+  rm src/amp_*.cpp
 fi
-if [ "$MATH_PATH" == "math" ]; then
-  MATH_PATH="/usr/local/bin/math"
-fi
-
-# add the Mathematica path into the required locations in the source files
-sed -i '' -e "s|.*MATH_PATH.*|    /\*MATH_PATH \*/  file<< \"#!$MATH_PATH/MathematicaScript -script\"<<endl;|g" src/templates.cpp
-
-sed -i '' -e "s|.*MATH_KERNEL_PATH.*|    /\*MATH_KERNEL_PATH \*/  WSTPflags << \"-linkname \" << \"$MATH_PATH/MathKernel\" << \" -mathlink\";|g" include/compute_amp.hpp
-
-echo "  "
-echo "Please enter path to directory containing the TSIL header tsil_cpp.h"
-echo "for example /Users/<user_name>/Programs/tsil-1.3"
-read TSIL_PATH
-
-# following is default for my system, could use find / -name tsil_cpp.h to search but this is slow
-if [ "$TSIL_PATH" == d ]; then
-  TSIL_PATH=/Users/jamesmckay/Documents/Programs/tsil-1.3
-fi
-
-# add the TSIL path into the required places
-sed -i '' -e "s|.*TSIL_INCLUDE_PATH.*|  /\*TSIL_INCLUDE_PATH \*/#include \"$TSIL_PATH/tsil_cpp.h\"  // Required TSIL header file|g" src/*.cpp
-
-sed -i '' -e "s|.*TSIL_PATH.*|    /\* TSIL_PATH \*/ std::string TSIL = \"$TSIL_PATH/tsil_cpp.h\";  |g" src/*.cpp
-
-sed -i '' -e "s|.*set(TSIL_HEADER_FILE.*|set(TSIL_HEADER_FILE $TSIL_PATH/tsil_cpp.h ) |g" CMakeLists.txt
-
-sed -i '' -e "s|.*LINK_DIRECTORIES(.*|LINK_DIRECTORIES($TSIL_PATH) |g" CMakeLists.txt
-
-
-
 
 # create bare self_energy.cpp so compile works before actual code generated
 cp src/self_energy_bak.cpp src/self_energy.cpp
 cp include/self_energy_backup.hpp include/self_energy.hpp
 cp include/data_bak.hpp include/data.hpp
-#rm src/amp_*.cpp
-# create working directories and build directory
-mkdir build
-mkdir output
-mkdir models/EW_triplet/output
-mkdir models/Scalar/output
 
+# create working directories and build directory
+
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+if [ ! -d "output" ]; then
+  mkdir output
+fi
+if [ ! -d "models/EW_triplet/output" ]; then
+  mkdir models/EW_triplet/output
+fi
+if [ ! -d "models/Scalar/output" ]; then
+  mkdir models/Scalar/output
+fi
+if [ ! -d "models/MSSM/output" ]; then
+  mkdir models/MSSM/output
+fi
+if [ ! -d "models/VDM/output" ]; then
+  mkdir models/VDM/output
+fi
