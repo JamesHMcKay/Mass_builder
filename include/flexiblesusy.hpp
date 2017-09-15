70052ba09efdff8836b1860633512384d25681d4
James McKay <j.mckay14@imperial.ac.uk>
2017-09-15 17:07:39 +1200

    added some missing untracked files

---
+++ include/flexiblesusy.hpp
@@ -0,0 +1,34 @@
+#ifndef FLEXIBLESUSY_HPP
+#define FLEXIBLESUSY_HPP
+
+#include "flexiblesusy/SLHAea/slhaea.h"
+#include "flexiblesusy/src/lowe.h"
+#include "flexiblesusy/src/utils.h"
+
+#define ALGORITHM1 Two_scale
+
+#include "flexiblesusy/models/MDM/MDM_input_parameters.hpp"
+#include "flexiblesusy/models/MDM/MDM_slha_io.hpp"
+#include "flexiblesusy/models/MDM/MDM_spectrum_generator.hpp"
+#include "flexiblesusy/models/MDM/MDM_two_scale_model.hpp"
+#include "flexiblesusy/models/MDM/MDM_two_scale_model_slha.hpp"
+#include "flexiblesusy/models/MDM/MDM_physical.hpp"
+#include "flexiblesusy/models/MDM/MDM_info.hpp"
+
+#include "flexiblesusy/models/EW_triplet/EW_triplet_input_parameters.hpp"
+#include "flexiblesusy/models/EW_triplet/EW_triplet_slha_io.hpp"
+#include "flexiblesusy/models/EW_triplet/EW_triplet_spectrum_generator.hpp"
+#include "flexiblesusy/models/EW_triplet/EW_triplet_two_scale_model.hpp"
+#include "flexiblesusy/models/EW_triplet/EW_triplet_two_scale_model_slha.hpp"
+#include "flexiblesusy/models/EW_triplet/EW_triplet_physical.hpp"
+#include "flexiblesusy/models/EW_triplet/EW_triplet_info.hpp"
+
+#include "flexiblesusy/models/MSSM/MSSM_input_parameters.hpp"
+#include "flexiblesusy/models/MSSM/MSSM_slha_io.hpp"
+#include "flexiblesusy/models/MSSM/MSSM_spectrum_generator.hpp"
+#include "flexiblesusy/models/MSSM/MSSM_two_scale_model.hpp"
+#include "flexiblesusy/models/MSSM/MSSM_two_scale_model_slha.hpp"
+#include "flexiblesusy/models/MSSM/MSSM_physical.hpp"
+#include "flexiblesusy/models/MSSM/MSSM_info.hpp"
+
+#endif
